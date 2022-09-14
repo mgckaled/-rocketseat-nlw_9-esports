653c974b650877e0d4f13751fe98df4b90110d3d
mgckaled <marcelgckaled@gmail.com>
2022-09-14 05:33:50 -0300

    fstage 2.6

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_900Black
+} from '@expo-google-fonts/inter'
+
+import { Background, Loading } from './src/components'
+import { Home } from './src/screens'
+
+const App: React.FC = () => {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_900Black
+	})
+
+	return (
+		<Background>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			{fontsLoaded ? <Home /> : <Loading />}
+		</Background>
+	)
+}
+
+export default App
---
+++ server/notes.md
@@ -0,0 +1,5 @@
+# Informações importantes
+
+- O node não interpreta typescript em processo de produção. Logo, será necessário instalar o módulo em condição de desenvolvimento.
+
+- npx: serve para executar arquivo instalado em biblioteca.
